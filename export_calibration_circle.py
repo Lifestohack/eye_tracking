0e7fa26f12baf2314f9c64a2ac156dbf2d2e9f11
Diwas Bhattarai <diwas@bhattarai.de>
2020-12-13 20:47:08 +0100

    formatted export_calibration_circle

--- export_calibration_circle.py
+++ export_calibration_circle.py
@@ -4,23 +4,25 @@ import cv2
 from external.circle_detector import find_pupil_circle_marker
 import csv
 
+
 def get_samples(path):
     samples = os.listdir(path)
     samples = [os.path.join(path, sample) for sample in samples if "sample" in sample]
     return samples
 
+
 def get_raw_data(sample):
     world_index = 0
     cap = cv2.VideoCapture(sample)
     total_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    while(cap.isOpened()):
+    while cap.isOpened():
         ret, frame = cap.read()
         if ret is False:
             break
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         ellipses_list = find_pupil_circle_marker(gray, scale=1)
         world_index += 1
-        print((world_index/total_frame)*100)
+        print((world_index / total_frame) * 100)
         marker_list = []
         del frame, gray
         for ellipses_ in ellipses_list:
@@ -34,10 +36,11 @@ def get_raw_data(sample):
                 }
             )
             yield marker_list
-        #cv2.imshow('frame',gray)
-        #if cv2.waitKey(1) & 0xFF == ord('q'):
+        # cv2.imshow('frame',gray)
+        # if cv2.waitKey(1) & 0xFF == ord('q'):
         #    break
     cap.release()
+
 
 def export_csv(sample):
     video_sample = os.path.join(sample, "world.mp4")
@@ -57,5 +60,5 @@ def export_csv(sample):
 
 if __name__ == "__main__":
     samples_path = "data"
-    samples = get_samples(samples_path) 
+    samples = get_samples(samples_path)
     export_csv(samples[-1])
