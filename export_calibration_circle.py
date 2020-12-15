35bb23d95a897ea6908718f36901ef42807fa0c7
Diwas Bhattarai <diwas@bhattarai.de>
2020-12-15 18:22:49 +0100

    create calibration csv file for all samples using circle_detector1.py

--- export_calibration_circle.py
+++ export_calibration_circle.py
@@ -4,21 +4,25 @@ import cv2
 from external.circle_detector import find_pupil_circle_marker
 import csv
 from helper import get_sample_paths, get_frames, find_circle_marker
+from external.circle_detector1 import (
+    find_pupil_circle_marker as find_pupil_circle_marker1,
+)
 
 
 def get_raw_data(sample):
     for world_index, frame in enumerate(get_frames(sample)):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        #ellipses_list = find_pupil_circle_marker(gray, scale=1)
-        ellipses_list = find_circle_marker(frame)  
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # ellipses_list = find_pupil_circle_marker(gray, scale=1)
+        # ellipses_list = find_circle_marker(frame)
+        ellipses_list = find_pupil_circle_marker1(frame)
         marker_list = []
         for ellipses_ in ellipses_list:
-            ellipses = ellipses_["ellipses"]
+            ellipses = ellipses_["ellipse"]
             img_pos = ellipses[0][0]
             marker_list.append(
                 {
                     "world_index": world_index,
-                    "ellipses_center": img_pos,
+                    "ellipse": img_pos,
                     "marker_type": ellipses_["marker_type"],
                 }
             )
@@ -33,7 +37,7 @@ def export_raw_data_to_csv(sample, save_file_name):
         for marker in markers:
             markers_list.append(marker)
 
-    with open(save_path, "a", newline="") as csvfile:
+    with open(save_path, "w", newline="") as csvfile:
         fieldnames = [
             "world_index",
             "ellipses_center",
@@ -45,15 +49,20 @@ def export_raw_data_to_csv(sample, save_file_name):
             writer.writerow(marker)
 
 
-if __name__ == "__main__":
-    marker_filename = "calibration_markers.csv"
+def get_samples(marker_filename, overwrite=False):
     all_samples = get_sample_paths()
+    if overwrite:
+        return all_samples
     samples_to_detect_markers = []
     for sample in all_samples:
-        contains = False
         filenames = os.listdir(sample)
         if not marker_filename in filenames:
             samples_to_detect_markers.append(sample)
+    return samples_to_detect_markers
 
 
-    export_raw_data_to_csv(samples_to_detect_markers[-1], marker_filename)
+if __name__ == "__main__":
+    marker_filename = "calibration_markers.csv"
+    sampels = get_samples(marker_filename, False)
+    for sample in sampels:
+        export_raw_data_to_csv(sample, marker_filename)
